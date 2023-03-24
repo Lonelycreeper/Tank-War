e18121a380a90af961bf20a4937b415508fd7eb3
Lonelycreeper <108966569+Lonelycreeper@users.noreply.github.com>
2023-03-24 20:36:08 +0800

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -14,7 +14,7 @@ class EmeCar(pygame.sprite.Sprite):
     def update(self):
         posi=self.rect
         self.image=pygame.image.load("./img/hehe.png").convert_alpha()
-        self.move=random.randint(1,4)
+        self.move=random.randint(1,2)
         if xy[0]<self.xy[0]:
                 self.xy[0]-=self.V
                 pygame.time.wait(ST2)
@@ -27,6 +27,20 @@ class EmeCar(pygame.sprite.Sprite):
         if xy[1]<self.xy[1]:
                 self.xy[1]-=self.V
                 pygame.time.wait(ST2)
+        if xy[0]+10 ==self.xy[0]:
+                if self.move==1:
+                    for x in range(200):
+                        self.xy[1]+=1
+                else:
+                    for x in range(200):
+                        self.xy[1]-=1
+        if xy[1]+10==self.xy[1]:
+                if self.move==1:
+                    for x in range(200):
+                        self.xy[0]-=1
+                else:
+                    for x in range(200):
+                        self.xy[0]+=1    
         self.posi=posi.move(self.xy)
     def Create(self):
         if self.re==0:
